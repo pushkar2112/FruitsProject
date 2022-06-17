84e518dc48fe89e9ed7ee5ec527c81d041e7c5c1
Pushkar Verma <pushkar7397@gmail.com>
2022-06-17 13:43:38 +0530

    find data from the database

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ client.connect(function(err) {
 
   const db = client.db(dbName);
 
-  insertDocuments(db, function(){
+  findDocuments(db, function(){
     client.close();
   })
   
@@ -50,5 +50,17 @@ const insertDocuments = function(db, callback) {
 // https://stackoverflow.com/questions/68474512/mongodb-nodejs-error-assert-equal3-result-result-n-not-working-giving-error
       console.log("Inserted 3 documents into the collection");
       callback(result);
+    });
+  }
+
+  const findDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Find some documents
+    collection.find({}).toArray(function(err, fruits) {
+      assert.equal(err, null);
+      console.log("Found the following records");
+      console.log(fruits)
+      callback(fruits);
     });
   }
