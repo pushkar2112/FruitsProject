754990fc13b4e6081bc28af63253fe37cce89a5f
Pushkar Verma <pushkar7397@gmail.com>
2022-06-23 09:53:18 +0530

    Create schema, model and add record using mongoose

--- app.js
+++ app.js
@@ -2,37 +2,23 @@ const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/fruitsDB")
 
+const fruitSchema = new mongoose.Schema ({
+  name: String,
+  rating: Number,
+  review: String
+});
 
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit ({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit!"
+});
+
+fruit.save()
  
-const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('fruits');
-    // Insert some documents
-    collection.insertMany([
-        {
-            name : "Apple",
-            score: 8,
-            review: "Great Fruit!"
-        },
-        {
-            name : "Orange",
-            score: 6,
-            review: "Kinda Sour"
-        },
-        {
-            name : "Banana",
-            score: 9,
-            review: "Great Stuff!"
-        }
-    ], function(err, result) {
-      assert.equal(err, null);
-      assert.equal(3,result.insertedCount);
-      assert.equal(3,Object.keys(result.insertedIds).length);
-// https://stackoverflow.com/questions/68474512/mongodb-nodejs-error-assert-equal3-result-result-n-not-working-giving-error
-      console.log("Inserted 3 documents into the collection");
-      callback(result);
-    });
-  }
+
 
   const findDocuments = function(db, callback) {
     // Get the documents collection
