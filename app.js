8fe259bff130279de60c89fb0703fc6563f13f25
Pushkar Verma <pushkar7397@gmail.com>
2022-06-23 09:47:33 +0530

    create db using mongoose and remove driver code

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
 const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+mongoose.connect("mongodb://localhost:27017/fruitsDB")
 
-// Database Name
-const dbName = 'fruitsDB';
 
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  findDocuments(db, function(){
-    client.close();
-  })
-  
-});
  
 const insertDocuments = function(db, callback) {
     // Get the documents collection
