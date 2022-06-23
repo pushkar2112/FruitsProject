7b6c28c7bce507d5025bd04ab100d3663161ce4d
Pushkar Verma <pushkar7397@gmail.com>
2022-06-23 10:46:21 +0530

    Find data from the collection

--- app.js
+++ app.js
@@ -51,24 +51,20 @@ const banana = new Fruit({
   review: "It's Good!"
 });
 
-Fruit.insertMany([kiwi, orange, banana], function(err){
-  if (err) {
+// Fruit.insertMany([kiwi, orange, banana], function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Succefully saved all the fruits to fruitsDB");
+//   }
+// });
+
+Fruit.find(function(err, fruits){
+  if (err){
     console.log(err);
   } else {
-    console.log("Succefully saved all the fruits to fruitsDB");
-  }
-});
-
-
-
-  const findDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('fruits');
-    // Find some documents
-    collection.find({}).toArray(function(err, fruits) {
-      assert.equal(err, null);
-      console.log("Found the following records");
-      console.log(fruits)
-      callback(fruits);
+    fruits.forEach(function(fruit){
+      console.log(fruit.name);
     });
   }
+});
