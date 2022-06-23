bd880ebf574d825df60f691a5b284a0b2e50390f
Pushkar Verma <pushkar7397@gmail.com>
2022-06-23 09:45:37 +0530

    Migrate to mongoose from native driver

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
